badb372f07202a542b9b04927c6d4b19bca893d7
victorclee <victorchristopherlee@gmail.com>
2020-02-05 11:55:43 -0600

    Initial commit.

---
+++ assets/scripts/objects.js
@@ -0,0 +1,29 @@
+// const movieList = document.getElementById('movie-list');
+
+// movieList.style['background-color'] = 'red';
+// movieList.style.display = 'block';
+
+// const userChosenKeyName = 'level';
+
+// let person = {
+//   'first name': 'Victor',
+//   age: 35,
+//   hobbies: ['Lifting', 'Coding', 'Hiking'],
+//   [userChosenKeyName]: '...',
+//   greet: function() {
+//     alert('Hi there!');
+//   },
+//   1.5: 'hello'
+// };
+
+// // person.age = 37;
+// delete person.age;
+// // person.age = undefined;
+// // person.age = null;
+// person.isAdmin = true;
+
+// const keyName = 'first name';
+
+// console.log(person[keyName]);
+// console.log(person[1.5]);
+// console.log(person);

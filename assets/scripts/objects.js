5f9da740747b5f0a3535463386a790f7c72d61c3
victorclee <victorchristopherlee@gmail.com>
2020-02-24 13:56:49 -0600

    Swapped call with apply. Would act the same since Im only passing the first argument.

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -25,8 +25,8 @@ const renderMovies = (filter = '') => {
     console.log(otherProps);
     // const { title: movieTitle } = info;
     let { getFormattedTitle } = movie;
-    getFormattedTitle = getFormattedTitle.bind(movie);
-    let text = getFormattedTitle() + ' - ';
+    // getFormattedTitle = getFormattedTitle.bind(movie);
+    let text = getFormattedTitle.apply(movie) + ' - ';
     for (const key in info) {
       if (key !== 'title') {
         text = text + `${key}: ${info[key]}`;

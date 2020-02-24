8795323b673c1c189d4058e72d93e4fea5297c77
victorclee <victorchristopherlee@gmail.com>
2020-02-24 14:12:15 -0600

    Tracking this to the DOM element

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -66,7 +66,8 @@ const addMovieHandler = () => {
   renderMovies();
 };
 
-const searchMovieHandler = () => {
+const searchMovieHandler = function() {
+  console.log(this);
   const filterTerm = document.getElementById('filter-title').value;
   renderMovies(filterTerm);
 };

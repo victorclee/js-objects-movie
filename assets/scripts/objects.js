570281c36cc3d35a601f7ed677004e40b9b1fab9
victorclee <victorchristopherlee@gmail.com>
2020-02-24 15:22:37 -0600

    Getters and setters

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -28,7 +28,7 @@ const renderMovies = (filter = '') => {
     // getFormattedTitle = getFormattedTitle.bind(movie);
     let text = getFormattedTitle.apply(movie) + ' - ';
     for (const key in info) {
-      if (key !== 'title') {
+      if (key !== 'title' && key !== '_title') {
         text = text + `${key}: ${info[key]}`;
       }
     }
@@ -43,7 +43,6 @@ const addMovieHandler = () => {
   const extraValue = document.getElementById('extra-value').value;
 
   if (
-    title.trim() === '' ||
     extraName.trim() === '' ||
     extraValue.trim() === ''
   ) {
@@ -52,7 +51,16 @@ const addMovieHandler = () => {
 
   const newMovie = {
     info: {
-      title,
+      set title(val) {
+        if (val.trim() === '') {
+          this._title = 'DEFAULT';
+          return;
+        }
+        this._title = val;
+      },
+      get title() {
+        return this._title;
+      },
       [extraName]: extraValue
     },
     id: Math.random().toString(),
@@ -61,6 +69,9 @@ const addMovieHandler = () => {
       return this.info.title.toUpperCase();
     }
   };
+
+  newMovie.info.title = title;
+  console.log(newMovie.info.title);
 
   movies.push(newMovie);
   renderMovies();

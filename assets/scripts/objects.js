41b33748da43c3c8dc4f3b654428a0748981ac44
victorclee <victorchristopherlee@gmail.com>
2020-02-24 13:48:16 -0600

    Working with this and bind

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,3 +1,4 @@
+"use strict";
 const addMovieBtn = document.getElementById('add-movie-btn');
 const searchBtn = document.getElementById('search-btn');
 
@@ -23,8 +24,9 @@ const renderMovies = (filter = '') => {
     const { info, ...otherProps } = movie;
     console.log(otherProps);
     // const { title: movieTitle } = info;
-    // const { getFormattedTitle } = movie;
-    let text = movie.getFormattedTitle() + ' - ';
+    let { getFormattedTitle } = movie;
+    getFormattedTitle = getFormattedTitle.bind(movie);
+    let text = getFormattedTitle() + ' - ';
     for (const key in info) {
       if (key !== 'title') {
         text = text + `${key}: ${info[key]}`;
@@ -55,6 +57,7 @@ const addMovieHandler = () => {
     },
     id: Math.random().toString(),
     getFormattedTitle() {
+      console.log(this);
       return this.info.title.toUpperCase();
     }
   };
